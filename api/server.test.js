5333235615b48c16f6abe4fa773b896ab7ea0db5
Rebecca-Stone <primstone16@gmail.com>
2022-04-28 21:03:03 -0500

    setting up test layout

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,31 @@
-// - [ ] A minimum of 2 tests per API endpoint, written inside `api/server.test.js`.
+// import the servers
+// import supertest
+// import db
+// import restricted
+// import other middleware
+
+// beforeAll(async () => { });
+
+// beforeEach(async () => { });
+
+// afterAll(async () => { });
 
 // Write your tests here
 test("sanity", () => {
   expect(true).toBe(true);
 });
+
+// test 1 & 2
+// ---------------------------------------------------------------
+// test('[1] POST/api/auth/register -returns a new user', () => {});
+// test('[2] POST/api/auth/register -the user is found within all users', () => {});
+
+// test 3 & 4
+// ---------------------------------------------------------------
+// test('[3] POST/api/auth/login - logs an existing user in', () => {});
+// test('[4] POST/api/auth/login - logged in users can see dad jokes', () => {});
+
+// test 5 & 6
+// ---------------------------------------------------------------
+// test('[5] GET/api/jokes/ -jokes are visible to existing users', () => {});
+// test('[6] GET/api/jokes/ -', () => {});
